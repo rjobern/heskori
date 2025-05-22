539224ac120ea9fe3627155441141a567dc4f964
rjobern <rjobern@gmail.com>
2025-05-22 12:44:14 +0200

    Erste Änderungen

---
+++ admin/custom.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ *
+ * This file is part of HESK - PHP Help Desk Software.
+ *
+ * (c) Copyright Klemen Stirn. All rights reserved.
+ * https://www.hesk.com
+ *
+ * For the full copyright and license agreement information visit
+ * https://www.hesk.com/eula.php
+ *
+ */
+
+define('IN_SCRIPT',1);
+define('HESK_PATH','../');
+
+/* Get all the required files and functions */
+require(HESK_PATH . 'hesk_settings.inc.php');
+require(HESK_PATH . 'inc/common.inc.php');
+require(HESK_PATH . 'inc/admin_functions.inc.php');
+hesk_load_database_functions();
+
+hesk_session_start();
+hesk_dbConnect();
+hesk_isLoggedIn();
+
+/* Check permissions for this feature */
+hesk_checkPermission('can_view_tickets');
+hesk_checkPermission('can_reply_tickets');
+
+/* A security check */
+hesk_token_check('POST');
+
+/* Ticket ID */
+var_dump( $_POST );
+$trackingID = hesk_cleanID() or die($hesklang['int_error'].': '.$hesklang['no_trackID']);
+$customvalue=hesk_post('customvalue');
+$customname=hesk_post('customName');
+$customId=hesk_post('customId');
+$revision = sprintf($customname .$hesklang['changed'],hesk_date(),addslashes($_SESSION['name']).' ('.$_SESSION['user'].')');
+
+hesk_dbQuery("UPDATE `".hesk_dbEscape($hesk_settings['db_pfix'])."tickets` SET  `$customId`='{$customvalue}', `history`=CONCAT(`history`,'".hesk_dbEscape($revision)."') WHERE `trackid`='".hesk_dbEscape($trackingID)."'");
+
+
+if (hesk_dbAffectedRows() ==1)
+{
+hesk_process_messages($customname .$hesklang['changed'],'admin_ticket.php?track='.$trackingID.'&Refresh='.mt_rand(10000,99999),'NOTICE');
+}
+
+?>
